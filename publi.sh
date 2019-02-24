8346f230635aee1d62ff208298e03202b97baf9d
Lucy Davinhart <lucy@lucymhdavies.com>
2019-02-24 23:05:16 +0000

    Bit more bootstrap in the main index page

--- publi.sh
+++ publi.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
-echo "<h1>LMHD Labs</h1>" > index.htm
+cat << EOF > index.htm
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<style>
+.project img {
+	width: 256px;
+}
+
+.card {
+	display: inline-block;
+	vertical-align: top;
+}
+
+.jumbotron {
+	background: #cc0066;
+	color: white;
+}
+</style>
+
+<div class="jumbotron">
+  <h1 class="display-4">LMHD Labs</h1>
+</div>
+
+<div class="container">
+
+EOF
 
 for d in */ ; do
 	echo $d
-
-	cat << EOF >> index.htm
-<h2><a href="${d}">${d}</a></h1>
-EOF
 
 	for s in ${d}screenshot* ; do
 		if [ -f $s ]; then
 			echo $s
 
 			cat << EOF >> index.htm
-<img src="/${s}" />
+<div class="card" style="width: 18rem;">
+	<a href="${d}"><img src="/${s}" class="card-img-top"></a>
+	<div class="card-body">
+		<h5 class="card-title"><a href="${d}">${d}</a></h5>
+	</div>
+</div>
 EOF
 		fi
 
 	done
 done
+
+cat << EOF >> index.htm
+</div>
+EOF
